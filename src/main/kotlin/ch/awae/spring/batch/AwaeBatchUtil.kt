547d62ad114d976e06946c99238e25ae786155da
Andreas Wälchli <andreas.waelchli@me.com>
2024-07-02 17:56:40 +0000

    release (3.3.1).1: default to no-web

--- src/main/kotlin/ch/awae/spring/batch/AwaeBatchUtil.kt
+++ src/main/kotlin/ch/awae/spring/batch/AwaeBatchUtil.kt
@@ -1,25 +1,34 @@
 package ch.awae.spring.batch
 
-import org.springframework.boot.SpringApplication
-import kotlin.system.exitProcess
+import org.springframework.boot.*
+import kotlin.system.*
 
-object AwaeBatchUtil {
+/**
+ * run batch with custom initializer.
+ *
+ * before the initializer is executed, the [SpringApplication.webApplicationType] set to [WebApplicationType.NONE]
+ */
+fun <T> runBatch(clazz: Class<T>, vararg args: String, initializer: SpringApplication.() -> Unit): Nothing {
+    val app = SpringApplication(clazz)
+    app.webApplicationType = WebApplicationType.NONE
+    initializer(app)
+    runBatch(app, *args)
+}
 
-    fun <T> runBatch(clazz: Class<T>, vararg args: String, initializer: SpringApplication.() -> Unit): Nothing {
-        val app = SpringApplication(clazz)
-        initializer(app)
-        runBatch(app, *args)
+/**
+ * run batch with [SpringApplication.webApplicationType] set to [WebApplicationType.NONE]
+ */
+fun <T> runBatch(clazz: Class<T>, vararg args: String): Nothing {
+    runBatch(clazz, *args) {
+        // no-op initializer
     }
+}
 
-    fun <T> runBatch(clazz: Class<T>, vararg args: String): Nothing {
-        val app = SpringApplication(clazz)
-        runBatch(app, *args)
-    }
-
-    fun runBatch(app: SpringApplication, vararg args: String): Nothing {
-        val ctx = app.run(*args)
-        val exc = SpringApplication.exit(ctx)
-        exitProcess(exc)
-    }
-
+/**
+ * run batch from pre-initialized [SpringApplication]
+ */
+fun runBatch(app: SpringApplication, vararg args: String): Nothing {
+    val ctx = app.run(*args)
+    val exc = SpringApplication.exit(ctx)
+    exitProcess(exc)
 }
--- src/main/kotlin/ch/awae/spring/batch/AwaeSkipPolicy.kt
+++ src/main/kotlin/ch/awae/spring/batch/AwaeSkipPolicy.kt
@@ -1,13 +1,11 @@
 package ch.awae.spring.batch
 
-import org.springframework.batch.core.step.skip.SkipPolicy
-import java.util.logging.Level
-import java.util.logging.Logger
+import org.springframework.batch.core.step.skip.*
 
 class AwaeSkipPolicy(private val skipLimit: Int = -1) : SkipPolicy {
 
-    private val logger = Logger.getLogger(javaClass.name)
     override fun shouldSkip(t: Throwable, skipCount: Long): Boolean {
         return (t is ItemSkipException) && (skipLimit < 0 || skipCount < skipLimit)
     }
+
 }
